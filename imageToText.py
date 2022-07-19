4788ba54d3ed7ab0f80055a756c93f8885c4c667
ahsantariq792 <tariqahsan63@gmail.com>
2022-07-20 01:25:46 +0500

    imagetotext

--- imageToText.py
+++ imageToText.py
@@ -2,7 +2,7 @@ import pytesseract as tess
 tess.pytesseract.tesseract_cmd = r'D:\Python\Lib\Tesseract\tesseract.exe'
 from PIL import Image
 
-img = Image.open("text.png")
+img = Image.open("text.png")  #give path of your file
 text = tess.image_to_string(img)
 
 print(text)
